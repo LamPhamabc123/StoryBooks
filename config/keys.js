69541512c334bc7343ae25a2b1f09502c5856116
=Lam Pham <=lpham27@csc.com>
2018-11-06 22:03:01 +0700

    handled auth

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI:'mongodb://lampham:123456abc123@ds113566.mlab.com:13566/storybooks-dev',
-    googleClientID:'114381439332-25hkiniq6tvc1atmi6ohke7uvjqj5dik.apps.googleusercontent.com',
-    googleClientSecret:'t5xpgj7W7y7GOs7pBOxwSPMl'
-  }
+  mongoURI:'mongodb://storybooks:123456abc123@ds153593.mlab.com:53593/storybooks',
+  googleClientID:'114381439332-25hkiniq6tvc1atmi6ohke7uvjqj5dik.apps.googleusercontent.com',
+  googleClientSecret:'t5xpgj7W7y7GOs7pBOxwSPMl'
+}
