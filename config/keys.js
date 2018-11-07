18f90ae866c03eb1e3115d7640f73251c4e6d980
=Lam Pham <=lpham27@csc.com>
2018-11-07 22:18:36 +0700

    Fetch

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,13 @@
-if(process.env.NODE_ENV === 'production'){
+// if(process.env.NODE_ENV === 'production'){
   module.exports = {
     mongoURI: 'mongodb://storybooks:123456abc123@ds153593.mlab.com:53593/storybooks',
     googleClientID:'114381439332-25hkiniq6tvc1atmi6ohke7uvjqj5dik.apps.googleusercontent.com',
     googleClientSecret:'t5xpgj7W7y7GOs7pBOxwSPMl'
   }
-} else {
-  module.exports = {
-    mongoURI: 'mongodb://localhost/lampham',
-    googleClientID:'114381439332-25hkiniq6tvc1atmi6ohke7uvjqj5dik.apps.googleusercontent.com',
-    googleClientSecret:'t5xpgj7W7y7GOs7pBOxwSPMl'
-  }
-}
+// } else {
+//   module.exports = {
+//     mongoURI: 'mongodb://localhost/lampham',
+//     googleClientID:'114381439332-25hkiniq6tvc1atmi6ohke7uvjqj5dik.apps.googleusercontent.com',
+//     googleClientSecret:'t5xpgj7W7y7GOs7pBOxwSPMl'
+//   }
+// }
